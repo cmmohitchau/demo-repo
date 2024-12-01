ed7386444018d085e97bf848bddf5286e496f0d0
cmmohitchau <cmmohitchau@gmail.com>
2024-12-01 15:54:29 +0530

    added request for body and response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
 
 const PORT = 3000;
 
-app.get('/' , (req , res) => {
-    res.send("hello world");
+app.get('/' , async (req , res) => {
+    const body = await req.body;
+
+    res.status(200).json({
+        username : body.username,
+        name : body.name
+    })
 })
 
 app.listen(PORT , () => {
