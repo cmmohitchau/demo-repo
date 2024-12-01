578d202ba4679dcd958a39440a6eff54727edeeb
cmmohitchau <cmmohitchau@gmail.com>
2024-12-01 15:42:32 +0530

    added simple  get endpoint for hello world

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+
+const PORT = 3000;
+
+app.get('/' , (req , res) => {
+    res.send("hello world");
+})
+
+app.listen(PORT , () => {
+    console.log(`app is listening on PORT${PORT}`);
+    
+})
